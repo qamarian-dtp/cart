006c7a85cb30a64a8e7e44ef2be387ae627d3139
Qamarian <qamarian.mail@gmail.com>
2019-08-08 21:27:58 +0000

    Progress 4

--- adminPanel.go
+++ adminPanel.go
@@ -9,12 +9,13 @@ func newAdminPanel (c *Cart) (*AdminPanel) {
 }
 
 // This data type is meant to support the Cart data type. Some priviledged methods that
-// can not be invoked directly on a Cart, can be invoked directly on this type.
+// can not be invoked directly on a Cart, can be invoked directly on this data type.
 type AdminPanel struct {
 	underlyingCart *Cart
 }
 
-// Harvest () is a wrapper around method harvest () of the Cart abstract data type.
+// Harvest () is a wrapper around method harvest () of the Cart abstract data type. If
+// the underlying cart haa already been harvested, value would be ErrBeenHarvested.
 func (p *AdminPanel) Harvest () (*list.List, error) {
 	return p.underlyingCart.harvest ()
 }
